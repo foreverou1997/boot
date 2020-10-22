ddc4ddc4585a6bc1e6f13d8dcd5ad18b36bf29ae
oulei <wb-ol702876@alibaba-inc.com>
2020-10-22 20:30:09 +0800

    集成rocketMq

---
+++ src/main/java/com/study/demo/boot/controller/rocketmq/TestMqController.java
@@ -0,0 +1,23 @@
+package com.study.demo.boot.controller.rocketmq;
+
+import com.study.demo.boot.domain.dto.UserDTO;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rocketMq")
+public class TestMqController {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @GetMapping("/test")
+    public String sendMsg(){
+        rocketMQTemplate.convertAndSend("test",new UserDTO("userId", "userName"));
+        return "消息发送成功";
+    }
+
+}
